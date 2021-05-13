7e655917524d52db77d20f291c5eb87de2fef5d4
Debo15 <40213379+Debo15@users.noreply.github.com>
2021-05-13 13:38:28 +0300

    implemented HeapSort and BubbleSort

--- Sort/BubbleSort.cpp
+++ Sort/BubbleSort.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
+template<class T>
+void bubbleSort(T* arr, size_t size){
+  for (size_t i = 0; i < size; i++){
+    for (size_t j = i+1; j < size; j++){
+      if (arr[i] > arr[j])swap(arr[i], arr[j]);
+    }
+  }
+}
+
+int main() {
+  int arr[] = {6, 3, 2, 9, 9, 10, 2, 23, -13, -4};
+  size_t size = sizeof(arr)/sizeof(arr[0]);
+  bubbleSort(arr, size);
+  for (size_t i = 0; i < size; i++)cout << arr[i] << ' ';
   return 0;
 }
--- Sort/HeapSort.cpp
+++ Sort/HeapSort.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
+template<class T>
+void heapify(T* arr, int size, int idx){ // size: get the max heap to this size (e.g [idx, size) )
 
-int main(){
+  int l = idx*2 + 1
+  , r = idx*2 + 2
+  , largest = idx;
+
+  if (l < size && arr[l] > arr[largest])largest = l;
+
+  if (r < size && arr[r] > arr[largest])largest = r;
+
+  if (largest != idx){
+    swap(arr[largest], arr[idx]);
+    heapify(arr, size, largest);
+  }
+  
+}
+
+template<class T>
+void heapSort(T* arr, size_t size){
+  
+  // build the max-heap
+  for (int i = size/2 - 1; i >= 0; i--) // start from the mid; to make sure that the current node does have a children.
+    heapify(arr, size, i);
+  // extract elements from the tree one-by-one
+  for (int i = size - 1; i >= 0; i--){
+
+    swap(arr[0], arr[i]);
+
+    heapify(arr, i, 0);
+  }
+}
+
+int main() {
+
+  int arr[] = {6, 3, 2, 9, 9, 10, 2, 23, -13, -4};
+  size_t size = sizeof(arr)/sizeof(arr[0]);
+
+  heapSort(arr, size);
+
+  for (size_t i = 0; i < size; i++)cout << arr[i] << ' ';
+
   return 0;
 }

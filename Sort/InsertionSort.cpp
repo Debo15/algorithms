2e80462ec45b8e86bf7db2fac05193ccc24ecf2d
Debo15 <40213379+Debo15@users.noreply.github.com>
2021-05-13 14:17:23 +0300

    implemented the InsertsionSort Algorithm

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int main(){
+template<class T>
+void insertionSort(T* arr, size_t size){
+  
+  for (size_t i = 1; i < size; i++){
+    T cur = arr[i];
+    int j = i-1;
+    while (j >= 0 && arr[j] > cur){
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = cur;
+  }
+}
+
+int main() {
+
+  int arr[] = {6, 3, 2, 9, 9, 10, 2, 23, -13, -4};
+  size_t size = sizeof(arr)/sizeof(arr[0]);
+
+  insertionSort(arr, size);
+
+  for (size_t i = 0; i < size; i++)cout << arr[i] << ' ';
+
   return 0;
 }

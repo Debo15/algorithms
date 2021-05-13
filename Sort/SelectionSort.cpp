079041056413b151790a4e7314d0a9ead2566582
Debo15 <40213379+Debo15@users.noreply.github.com>
2021-05-13 13:48:13 +0300

    Implemented the SelectionSort

--- Sort/SelectionSort.cpp
+++ Sort/SelectionSort.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
-int main(){
+template<class T>
+void selectionSort(T* arr, size_t size){
+  
+  for (size_t i = 0; i < size; i++){
+    size_t minimum_idx = i;
+    for (size_t j = i+1; j < size; j++){
+      if (arr[j] < arr[minimum_idx])
+        minimum_idx = j;
+    }
+    swap(arr[i], arr[minimum_idx]);
+  }
+}
+
+int main() {
+
+  int arr[] = {6, 3, 2, 9, 9, 10, 2, 23, -13, -4};
+  size_t size = sizeof(arr)/sizeof(arr[0]);
+
+  selectionSort(arr, size);
+
+  for (size_t i = 0; i < size; i++)cout << arr[i] << ' ';
+
   return 0;
 }

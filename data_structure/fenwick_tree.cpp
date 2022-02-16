73c38fcafaa2e7ee11504f7e7326cc84be2e57a6
Debo15 <40213379+Debo15@users.noreply.github.com>
2022-02-16 05:39:40 +0200

    added range update fenwick, updated fenwick

--- data_structure/fenwick_tree.cpp
+++ data_structure/fenwick_tree.cpp
@@ -6,15 +6,16 @@ using namespace std;
 template<class T>
 class fenwick_tree {
   vector<T> BIT;
-
+  int n;
 public:
 
   fenwick_tree(int n) {
-    BIT.resize(n);
+    BIT.resize(n+5);
+    this->n = n+5;
   }
 
-  void update(int idx, int value) {
-    while (idx < N) {
+  void update(int idx, T value) {
+    while (idx < n) {
       BIT[idx] += value;
       idx += idx & -idx;
     }
@@ -27,5 +28,24 @@ public:
       idx -= idx & -idx;
     }
     return res;
+  }
+};
+
+template<class T>
+class range_fenwick_tree {
+  fenwick_tree<T> M, C;
+
+public:
+  range_fenwick_tree(int n) : M(n), C(n){};
+
+  void update(int l, int r, T val) {
+    M.update(l, val);
+    M.update(r + 1, -val);
+    C.update(l, -val * (l - 1));
+    C.update(r + 1, val * r);
+  }
+
+  T operator[](int idx) {
+    return idx * M[idx] + C[idx];
   }
 };
